69ff7eeff3079e766a9c8ef7cf30d60376690aa4
Kristen-B <119834839+Kristen-B@users.noreply.github.com>
2022-12-19 22:16:40 +1030

    Create Sort_by_Height.py
    Question 11

---
+++ Sort_by_Height.py
@@ -0,0 +1,23 @@
+def solution(a):
+    
+    i=0
+    while (i<len(a)):
+        
+        if(a[i]==-1):
+            i=i+1
+            
+        else:
+            maxa=a[i]
+            
+            for j in range(i,len(a)):
+                
+                if(a[j]<maxa and a[j]!=-1):
+                    maxa=a[j]
+                    a[j]=a[i]
+                    a[i]=maxa
+                    
+            i=i+1
+            
+    return a
+            
+            
